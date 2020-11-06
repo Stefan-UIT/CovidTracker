605d1d519b6a98fd4d68cf9508cc18c0d8fe049e
Trung Vo <trung.vo@seldatinc.com>
2020-11-06 14:50:16 +0700

    base traanslate

--- CovidTracker/Networks/CovidService.swift
+++ CovidTracker/Networks/CovidService.swift
@@ -22,18 +22,10 @@ class CovidService {
 // MARK: - Movies Network Services
 extension CovidService: CovidNetworkable {
     func fetchSummaryStats(completion: @escaping (SummaryStats?, Error?) -> Void) {
-        provider.request(CovidTarget.fetchSummaryStats, completion: { (response) in
-            switch response {
-            case .failure(let error):
-                completion(nil, error)
-            case .success(let response):
-                do {
-                    let decodedData = try self.translationLayer.decode(SummaryStats.self, fromData: response.data)
-                    completion(decodedData, nil)
-                } catch let error {
-                    completion(nil, error)
-                }
-            }
+        provider.request(CovidTarget.fetchSummaryStats, completion: { [weak self] (response) in
+            guard let strongSelf = self else { return }
+            strongSelf.handleSingleData(response: response,
+                                  completion: completion)
         })
     }
     
@@ -43,13 +35,31 @@ extension CovidService: CovidNetworkable {
             case .failure(let error):
                 completion(nil, error)
             case .success(let response):
-                do {
+//                do {
                     let data = self.translationLayer.decodeSafelyArray(of: CountryStats.self, from: response.data)
                     completion(data, nil)
-                } catch let error {
-                    completion(nil, error)
-                }
+//                } catch let error {
+//                    completion(nil, error)
+//                }
             }
         })
+    }
+}
+
+// Support functions
+extension CovidService {
+    func handleSingleData<T: Decodable>(response: Result<Moya.Response, MoyaError>,
+                                        completion: @escaping (T?, Error?) -> Void) {
+        switch response {
+        case .failure(let error):
+            completion(nil, error)
+        case .success(let response):
+            do {
+                let decodedData = try self.translationLayer.decode(T.self, fromData: response.data)
+                completion(decodedData, nil)
+            } catch let error {
+                completion(nil, error)
+            }
+        }
     }
 }
